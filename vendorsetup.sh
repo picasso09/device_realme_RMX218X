2ef9c95c373e83b74c9ac7148e5474955fed630c
picasso09 <119201284+picasso09@users.noreply.github.com>
2024-07-01 13:31:52 +0000

    RMX2185: add vendorsetup
    Signed-off-by: picasso09 <119201284+picasso09@users.noreply.github.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,3 @@
-echo "Cloning Vendor"
 git clone --depth=1 https://github.com/picasso09/vendor_tree_RMX2185 vendor/realme/RMX2185
-echo "Cloning Kernel"
-git clone --depth=1 https://github.com/picasso09/Dv-kernelRMX2189Q kernel/realme/RMX2185
-echo "Done Go to cook"
+git clone --depth=1 https://github.com/picasso09/Dv-kernelRMX2189Q -b wifi kernel/realme/RMX2185
+
